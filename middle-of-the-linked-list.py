1c789bf9c5fa919934b9cf7b4ee80ef99968b821
Chinthala Varnitha <chinthalavarnitha@gmail.com>
2024-07-19 22:48:45 +0530

    Create middle-of-the-linked-list.py
    Using Slow and Fast Pointers finding the middle of the linked list

---
+++ middle-of-the-linked-list.py
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* middleNode(struct ListNode* head) {
+    struct ListNode* slow = head;
+    struct ListNode* fast = head->next;
+    while(fast!=NULL && fast->next!=NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    if(fast!=NULL){
+        return slow->next;
+    }
+    else{
+    return slow;
+    }
+}
